9cf276a852cc25aa987bc4a38c7fca30efc22f01
TKadoi <diohabara@gmail.com>
2022-03-03 13:25:03 +0900

    feat: initial commit

---
+++ main.v
@@ -0,0 +1,5 @@
+(** [plus] returns the sum of two natural numbers. *)
+(** Definition equals function. *)
+Definition plus (n : nat)(m : nat) : nat := n + m.
+
+Eval compute in fun (n:nat) => n * 2.
